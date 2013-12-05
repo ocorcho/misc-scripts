b5fb001bb32a47c8c4305b90e40cbc2c99b7058d
Nandana Mihindukulasooriya <nandana.cse@gmail.com>
2013-12-06 00:49:27 +0100

    Initial scripts for mirroring the github repos

---
+++ oeg-sync/create-mirror.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2012-2013 Ontology Engineering Group,
+#                         Universidad Politécnica de Madrid, Spain
+# 	http://www.oeg-upm.net/
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# 	http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+USER=nandana
+PASSWORD=
+REPO_URL=$1
+REGEX="^https:\/\/github.com/([a-zA-Z0-9_\.-]+)/([a-zA-Z0-9_\.-]+)$"
+
+display_usage() { 
+    echo "This script creates a mirror repository in https://github.com/oeg-upm" 
+    echo "Usage: create-mirror.sh <original-repo-url> "
+    echo "Eg. create-mirror.sh https://github.com/nandana/misc-scripts"
+} 
+
+# check for the number of parameters
+if [  $# -le 0 ] 
+ then 
+  display_usage
+  exit 1
+fi 
+
+#validate the repository argument and extract the name
+if [[ $REPO_URL =~ $REGEX ]]; then
+    REPO=${1##*/}
+    echo "Cloning the repository '$REPO'"
+else
+    echo "The repository parameter '$1' does not match the pattern https://github.com/<user>/<repo>"
+    exit 1
+fi
+
+# create an empty repository 
+curl -u ''$USER:$PASSWORD'' https://api.github.com/user/repos -d '{"name":"'$REPO'"}'
+
+# create a mirrored clone
+git clone --mirror $REPO_URL.git
+# set the url to make the push easier
+cd $REPO.git
+git remote set-url --push origin https://$USER:$PASSWORD@github.com/$USER/$REPO
+# Update the mirror
+git fetch -p origin
+git push --mirror
+
